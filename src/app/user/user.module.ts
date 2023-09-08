5727f78d22a16fb013f1a5671abd18c045d6fe8d
BiancaBoangiu <bianca_boangiu@yahoo.com>
2023-09-08 16:59:54 +0300

    commit

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
-import { UserComponent } from './components/dashboard/user.component';
+import { AccountComponent } from './components/account/account.component';
+import { UserBannerComponent } from './components/account-banner/user-banner.component';
+import { UserMenuComponent } from './components/account-navbar/user-menu.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [
+    AccountComponent,
+    UserBannerComponent,
+    UserMenuComponent,
+    DashboardComponent,
+  ],
   imports: [CommonModule, UserRoutingModule],
 })
 export class UserModule {}
