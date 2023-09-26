96398a5b0816453cefabf5ebb0701e81e14867a5
BiancaBoangiu <bianca_boangiu@yahoo.com>
2023-09-26 16:02:33 +0300

    commit

--- src/app/user/components/payment/payment.component.ts
+++ src/app/user/components/payment/payment.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { AuthService } from 'src/app/auth/services/auth.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
@@ -17,53 +23,101 @@ export class PaymentComponent {
     private authService: AuthService,
     private fb: FormBuilder
   ) {
-    this.paymentForm = this.fb.group({
-      cardNumber: ['', Validators.required],
-      cardYear: ['', Validators.required],
-      cardMonth: ['', Validators.required],
-      cardCvv: ['', Validators.required],
-      cardName: ['', Validators.required],
-    });
+    this.paymentForm = this.fb.group(
+      {
+        cardNumber: ['', Validators.required],
+        cardYear: ['', Validators.required],
+        cardMonth: ['', Validators.required],
+        cardCvv: ['', Validators.required],
+        cardName: ['', Validators.required],
+      },
+      {
+        validator: [
+          this.cardNumberLengthValidator,
+          this.monthValidator,
+          this.yearValidator,
+          this.cvvValidator,
+        ],
+      }
+    );
   }
 
   ngOnInit() {
-    this.getPaymentInfo();
+    const paymentInfo = this.authService.getUserData()?.payment;
+    if (paymentInfo) {
+      this.getPaymentInfo();
+    }
   }
 
   getPaymentInfo() {
     const paymentInfo = this.authService.getUserData()?.payment;
 
-    if (paymentInfo) {
-      this.paymentForm.patchValue({
-        cardNumber: paymentInfo.cardNumber,
-        cardYear: paymentInfo.cardYear,
-        cardMonth: paymentInfo.cardMonth,
-        cardCvv: paymentInfo.cardCvv,
-        cardName: paymentInfo.cardName,
-      });
-    }
+    this.paymentForm.patchValue({
+      cardNumber: paymentInfo?.cardNumber,
+      cardYear: paymentInfo?.cardYear,
+      cardMonth: paymentInfo?.cardMonth,
+      cardCvv: paymentInfo?.cardCvv,
+      cardName: paymentInfo?.cardName,
+    });
   }
 
   onSubmit() {
-    const userId = this.authService.getUserData()?.id as number;
-    const cardNumberValue = this.paymentForm.get('cardNumber')?.value;
-    const cardYearValue = this.paymentForm.get('cardYear')?.value;
-    const cardMonthValue = this.paymentForm.get('cardMonth')?.value;
-    const cardCvvValue = this.paymentForm.get('cardCvv')?.value;
-    const cardNameValue = this.paymentForm.get('cardName')?.value;
-    if (userId) {
-      this.usersService
-        .savePaymentInfo(
-          cardNumberValue,
-          cardYearValue,
-          cardMonthValue,
-          cardCvvValue,
-          cardNameValue,
-          userId
-        )
-        .subscribe(() => {
-          this.paymentAdded = true;
-        });
+    if (this.paymentForm.valid) {
+      const userId = this.authService.getUserData()?.id as number;
+      const cardNumberValue = this.paymentForm.get('cardNumber')?.value;
+      const cardYearValue = this.paymentForm.get('cardYear')?.value;
+      const cardMonthValue = this.paymentForm.get('cardMonth')?.value;
+      const cardCvvValue = this.paymentForm.get('cardCvv')?.value;
+      const cardNameValue = this.paymentForm.get('cardName')?.value;
+      if (userId) {
+        this.usersService
+          .savePaymentInfo(
+            cardNumberValue,
+            cardYearValue,
+            cardMonthValue,
+            cardCvvValue,
+            cardNameValue,
+            userId
+          )
+          .subscribe(() => {
+            this.paymentAdded = true;
+          });
+      }
+    }
+  }
+  monthValidator(control: AbstractControl): ValidationErrors | null {
+    const month = control.value;
+    if (month < 1 || month > 12) {
+      return { invalidMonth: true };
+    } else {
+      return null;
+    }
+  }
+
+  yearValidator(control: AbstractControl): ValidationErrors | null {
+    const currentYear = new Date().getFullYear();
+    const year = control.value;
+    if (year < currentYear) {
+      return { invalidYear: true };
+    } else {
+      return null;
+    }
+  }
+
+  cvvValidator(control: AbstractControl): ValidationErrors | null {
+    const cvv = control.value;
+    if (!/^\d{3}$/.test(cvv)) {
+      return { invalidCvv: true };
+    } else {
+      return null;
+    }
+  }
+  cardNumberLengthValidator(control: AbstractControl): ValidationErrors | null {
+    const cardNumber = control.value;
+    if ((cardNumber && cardNumber.length < 16) || cardNumber.length > 16) {
+      return { invalidCardNumberLength: true };
+    } else {
+      return null;
     }
   }
 }
