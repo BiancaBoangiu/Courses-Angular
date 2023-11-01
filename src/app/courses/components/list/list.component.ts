96cd47c0cefebc6fba44b7837403e64ae54fc6c1
BiancaBoangiu <bianca_boangiu@yahoo.com>
2023-11-01 13:34:31 +0200

    commit

--- src/app/courses/components/list/list.component.ts
+++ src/app/courses/components/list/list.component.ts
@@ -10,8 +10,18 @@ import { Course } from '../../models/course.interface';
 export class ListComponent {
   experienceLevels = ['All', 'Beginner', 'Intermediate', 'Advanced'];
   priceLevel = ['All', 'Free', 'Premium'];
+  categories = [
+    'All',
+    'Programming',
+    'Web Development',
+    'Mobile Development',
+    'Data Science and ML',
+    'Cybersecurity',
+    'Cloud Computing',
+  ];
   activeLevelButton: string = '';
   activePriceButton: string = '';
+  activeCategoryButton: string = '';
   courses: Course[] = [];
 
   sortSelection = '';
@@ -47,5 +57,20 @@ export class ListComponent {
     this.courseService.getCoursesByOption(option).subscribe((courses) => {
       this.courses = courses;
     });
+  }
+
+  showCoursesByCategory(category: string) {
+    this.activeCategoryButton = category;
+    if (category === 'All') {
+      this.courseService.getCourses().subscribe((courses) => {
+        this.courses = courses;
+      });
+    } else {
+      this.courseService
+        .getCoursesByExperience(category)
+        .subscribe((courses) => {
+          this.courses = courses;
+        });
+    }
   }
 }
