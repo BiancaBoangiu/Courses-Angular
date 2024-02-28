7a02ca6711836110893b7ba709bf48256472ae77
BiancaBoangiu <bianca_boangiu@yahoo.com>
2024-02-28 14:28:55 +0200

    COMMIT

--- src/app/user/components/dashboard/dashboard.component.ts
+++ src/app/user/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { CoursesService } from 'src/app/courses/services/courses.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,8 +9,32 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
   totalCourses!: number;
+  numberOfCertificates: number = 0;
 
   showTotalCourses(totalCourses: number) {
     this.totalCourses = totalCourses;
+  }
+
+  constructor(
+    private authService: AuthService,
+    private coursesService: CoursesService
+  ) {}
+
+  ngOnInit() {
+    this,
+      this.authService.loggedUser$.subscribe((user) => {
+        if (user && user.purchasedCourses.length > 0) {
+          this.coursesService
+            .getCoursesByIds(user.purchasedCourses)
+            .subscribe((courses) => {
+              courses.forEach((course) => {
+                if (course.hasCertificate) {
+                  this.numberOfCertificates++;
+                  console.log(this.numberOfCertificates);
+                }
+              });
+            });
+        }
+      });
   }
 }
